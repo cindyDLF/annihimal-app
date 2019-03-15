49498c833677cef858322c3f301156284958fda0
ERNOUL <iamkapitaan@gmail.com>
2019-03-15 14:26:04 +0100

    carousel working

---
+++ components/images.js
@@ -0,0 +1,6 @@
+export default {
+  longnose: require("../assets/images/longnose.png"),
+  maki: require("../assets/images/maki.jpg"),
+  meerkat: require("../assets/images/meerkat.jpg"),
+  tiger: require("../assets/images/tiger.jpg")
+};
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,99 +1,86 @@
-import React from 'react';
+import React, { Component } from "react";
 import {
+  ActivityIndicator,
   Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
   Text,
-  TouchableOpacity,
   View,
-} from 'react-native';
-import { WebBrowser } from 'expo';
+  StyleSheet,
+  Dimensions,
+  StatusBar,
+  Platform
+} from "react-native";
+import { Constants, Font } from "expo";
+import SideSwipe from "react-native-sideswipe"; // 1.3.0
 
-import { MonoText } from '../components/StyledText';
+import { Card } from "../components/Card";
 
-export default class HomeScreen extends React.Component {
+const { width } = Dimensions.get("window");
+
+const planets = [
+  { title: "Meerkat", value: "meerkat", abbr: "Some fun fact" },
+  { title: "LongNose", value: "longnose", abbr: "Some fun fact" },
+  { title: "Maki", value: "maki", abbr: "Some fun fact" },
+  { title: "Tiger", value: "tiger", abbr: "Some fun fact" }
+];
+
+export default class App extends Component {
   static navigationOptions = {
-    header: null,
+    title: "Home"
   };
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <View style={styles.welcomeContainer}>
-            <Image
-              source={
-                __DEV__
-                  ? require('../assets/images/robot-dev.png')
-                  : require('../assets/images/robot-prod.png')
-              }
-              style={styles.welcomeImage}
-            />
-          </View>
+  state = {
+    currentIndex: 0,
+    fontsLoaded: false
+  };
 
-          <View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
+  componentDidMount = async () => {
+    await Font.loadAsync({
+      dhurjati: require("../assets/fonts/Dhurjati-Regular.ttf"),
+      "inconsolata-regular": require("../assets/fonts/Inconsolata-Regular.ttf"),
+      "inconsolata-bold": require("../assets/fonts/Inconsolata-Bold.ttf"),
+      "libre-barcode-39": require("../assets/fonts/LibreBarcode39-Regular.ttf")
+    });
 
-            <Text style={styles.getStartedText}>Get started by opening</Text>
+    this.setState({ fontsLoaded: true });
+  };
 
-            <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-              <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
-            </View>
+  render = () => {
+    const offset = (width - Card.WIDTH) / 2;
 
-            <Text style={styles.getStartedText}>
-              Change this text and your app will automatically reload.
-            </Text>
-          </View>
-
-          <View style={styles.helpContainer}>
-            <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-            </TouchableOpacity>
-          </View>
-        </ScrollView>
-
-        <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
-          </View>
-        </View>
+    return !this.state.fontsLoaded ? (
+      <View style={[styles.container, { justifyContent: "center" }]}>
+        <ActivityIndicator color="white" />
       </View>
-    );
-  }
-
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
-    }
-  }
-
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-  };
-
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
+    ) : (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        {/* <Image
+          resizeMode="contain"
+          style={styles.fill}
+          source={require("../assets/images/space-bg.jpg")}
+        /> */}
+        <SideSwipe
+          data={planets}
+          shouldCapture={() => true}
+          style={[styles.fill, { width }]}
+          contentContainerStyle={{ paddingTop: 100 }}
+          itemWidth={Card.WIDTH}
+          threshold={Card.WIDTH / 4}
+          extractKey={item => item.value}
+          contentOffset={offset}
+          onIndexChange={index =>
+            this.setState(() => ({ currentIndex: index }))
+          }
+          renderItem={({ itemIndex, currentIndex, item, animatedValue }) => (
+            <Card
+              animal={item}
+              index={itemIndex}
+              currentIndex={currentIndex}
+              animatedValue={animatedValue}
+            />
+          )}
+        />
+      </View>
     );
   };
 }
@@ -101,88 +88,32 @@ export default class HomeScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    alignItems: "stretch",
+    justifyContent: "flex-start",
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: "black"
   },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
+  fill: {
+    position: "absolute",
+    top: 0,
     left: 0,
     right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
+    bottom: 0
   },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
+  title: {
+    fontFamily: "dhurjati",
+    fontSize: 32,
+    color: "white",
+    backgroundColor: "transparent",
+    textAlign: "center",
+    marginTop: 8,
+    letterSpacing: 1.6,
+    zIndex: 2,
+    alignSelf: "center"
   },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
+  titlePlatformSpecific: Platform.select({
+    ios: {
+      marginBottom: 10
+    }
+  })
 });
