f5348dd97871a66ef8948423da23e0e553833897
De la fuente <delafuentecindy@gmail.com>
2019-06-27 14:04:27 +0200

    modify props title component

--- components/Title.js
+++ components/Title.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { Text, StyleSheet } from "react-native";
 
-const Title = ({ text, color, size, weight }) => {
+const Title = ({ text, color, size, weight, margin }) => {
   setStyle = (color, size) => {
     color = color || "#C3CC6A";
     size = size || 40;
     weight = weight || "bold";
+    margin = margin || 20;
     return {
       fontSize: size,
       padding: 5,
       fontFamily: "Avenir",
       fontWeight: weight,
       color: color,
-      margin: 20
+      margin: margin
     };
   };
   return <Text style={setStyle(color, size)}>{text}</Text>;
