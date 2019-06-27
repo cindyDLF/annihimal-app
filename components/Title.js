86981bb19d9c03fe9d345769b62bbe8ffed06a2c
De la fuente <delafuentecindy@gmail.com>
2019-06-27 13:37:26 +0200

    title component custom

--- components/Title.js
+++ components/Title.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { Text, StyleSheet } from "react-native";
 
-const Title = ({ text }) => <Text style={styles.text}>{text}</Text>;
+const Title = ({ text, color, size, weight }) => {
+  setStyle = (color, size) => {
+    color = color || "#C3CC6A";
+    size = size || 40;
+    weight = weight || "bold";
+    return {
+      fontSize: size,
+      padding: 5,
+      fontFamily: "Avenir",
+      fontWeight: weight,
+      color: color,
+      margin: 20
+    };
+  };
+  return <Text style={setStyle(color, size)}>{text}</Text>;
+};
 
 export default Title;
-
-const styles = StyleSheet.create({
-  text: {
-    fontSize: 40,
-    padding: 5,
-    fontFamily: "Avenir",
-    fontWeight: "bold",
-    color: "#C3CC6A",
-    margin: 20
-  }
-});
