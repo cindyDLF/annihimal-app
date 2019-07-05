504f9d2f50985048b9334bab80db9aabaceb39da
De la fuente <delafuentecindy@gmail.com>
2019-07-05 11:18:18 +0200

    feature/fixendpoint

--- api/endpoint.js
+++ api/endpoint.js
@@ -3,3 +3,5 @@ export const SUBSCRIBE_USER = "/api/users/subscribe";
 export const LOGIN_USER = "/api/users/login";
 export const LIST_USER_FAV = "/api/users/";
 export const FAVORITE = "/favorite";
+export const ANIMAL_LIST = "/api/animals";
+export const ANIMAL = "/api/animals/details/";
