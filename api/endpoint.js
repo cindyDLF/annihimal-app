32b49f15f673b7cdf22c17c85627bd4d3496350c
ERNOUL <iamkapitaan@gmail.com>
2019-07-05 09:50:21 +0200

    retrieving and formatting animal data

---
+++ api/endpoint.js
@@ -0,0 +1,5 @@
+export const BASE_URL = "https://annihimal-api.herokuapp.com/api";
+export const SUBSCRIBE_USER = "/users/subscribe";
+export const LOGIN_USER = "/users/login";
+export const ANIMAL_LIST = "/animals";
+export const ANIMAL = "/animals/details/";
--- constants/endpoint.js
+++
@@ -1,3 +0,0 @@
-export const BASE_URL = "https://annihimal-api.herokuapp.com";
-export const SUBSCRIBE_USER = "/api/users/subscribe";
-export const LOGIN_USER = "/api/users/login";
